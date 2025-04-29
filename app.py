f2669d0244aaa558d9535be7476b524960f31978
sajjadali56 <sajjad.ali@badriconsultancy.com>
2025-04-29 15:49:09 +0500

    Multi Page app using streamlit

--- app.py
+++ app.py
@@ -3,121 +3,24 @@ import streamlit_authenticator as stauth
 from streamlit_authenticator import Authenticate
 import yaml
 from yaml.loader import SafeLoader
-import pandas as pd
-
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-import numpy as np
-
-
-def get_data(sheet_name: str):
-    data = pd.read_excel("./budget.xlsx", sheet_name=sheet_name)
-    data.dropna(inplace=True)
-    data.fillna(0, inplace=True)
-    return data
 
 
 def main():
     authenticator.logout()
 
-    st.write("Session State:", st.session_state)
+    # Define the pages
+    main_page = st.Page("./pages/main_page.py", title="Main Page", icon="🎈")
+    page_2 = st.Page("./pages/page_2.py", title="Page 2", icon="❄️")
+    page_3 = st.Page("./pages/page_3.py", title="Page 3", icon="🎉")
 
-    import time
-
-    "Starting a long computation..."
-
-    # Add a placeholder
-    latest_iteration = st.empty()
-    bar = st.progress(0)
-
-    for i in range(100):
-        # Update the progress bar with each iteration.
-        latest_iteration.text(f"Iteration {i+1}")
-        bar.progress(i + 1)
-        time.sleep(0.1)
-
-    st.write(f'Welcome *{st.session_state.get("name")}*')
-    st.title("Some content")
-
-    map_data = pd.DataFrame(
-        np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4], columns=["lat", "lon"]
+    # Set up navigation
+    pg = st.navigation(
+        [main_page, page_2, page_3],
+        expanded=True,
     )
 
-    st.map(map_data)
-
-    x = st.slider("x")  # 👈 this is a widget
-    st.write(x, "squared is", x * x)
-
-    # Add a selectbox to the sidebar:
-    add_selectbox = st.sidebar.selectbox(
-        "How would you like to be contacted?", ("Email", "Home phone", "Mobile phone")
-    )
-
-    # Add a slider to the sidebar:
-    add_slider = st.sidebar.slider("Select a range of values", 0.0, 100.0, (25.0, 75.0))
-
-    chosen = st.sidebar.radio(
-        "Sorting hat",
-        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"),
-        horizontal=True,
-    )
-    st.sidebar.write(f"You are in {chosen} house!")
-
-    st.text_input("Enter any text", key="input")
-    st.write(st.session_state.get("input"))
-
-    budget = get_data("month")
-    # budget.sort_values("Budget", ascending=False, inplace=True)
-
-    st.markdown("## Monthly Budget")
-    st.write(f"{budget.iloc[3, 0]} = {budget.iloc[3,2]}")
-    st.dataframe(budget)
-
-    # sns plot here for Item agains Budget
-    left, right = st.columns(2)
-
-    fig, ax = plt.subplots()
-    ax = sns.barplot(
-        x="Item",
-        y="Budget",
-        data=budget,
-        palette="pastel",
-    )
-    ax.set(xlabel="Item", ylabel="Budget")
-    plt.xticks(rotation=90)
-    left.pyplot(fig)
-
-    fig, ax = plt.subplots()
-    ax = plt.pie(
-        budget["Budget"],
-        labels=budget["Item"].values,
-        autopct="%1.1f%%",
-        colors=sns.color_palette("pastel"),
-    )
-    right.pyplot(fig)
-
-    flat = get_data("flat")
-
-    st.markdown("## Flat")
-    st.write(f"{flat.iloc[3, 0]} = {flat.iloc[3,2]}")
-    st.dataframe(flat)
-
-    extra = get_data("extra")
-
-    st.markdown("## Extra")
-    st.dataframe(extra)
-
-    explode = [0.1, 0, 0, 0, 0]
-    fig, ax = plt.subplots()
-    ax = plt.pie(
-        extra["Budget"],
-        labels=extra["Item"].values,
-        explode=explode,
-        autopct="%1.1f%%",
-        colors=sns.color_palette("pastel"),
-    )
-    st.pyplot(fig)
+    # Run the selected page
+    pg.run()
 
 
 with open("./config.yaml") as file:
@@ -135,7 +38,10 @@ try:
     res = authenticator.login()
 
     if st.session_state.get("authentication_status"):
-        main()
+        # main()
+        authenticator.logout()
+        st.title("Welcome *{}*".format(st.session_state.get("name")))
+
     elif st.session_state.get("authentication_status") is False:
         st.error("Username or password is incorrect")
     elif st.session_state.get("authentication_status") is None:
---
+++ pages/main_page.py
@@ -0,0 +1,125 @@
+import streamlit as st
+import pandas as pd
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import numpy as np
+import time
+
+
+def get_data(sheet_name: str):
+    data = pd.read_excel("./budget.xlsx", sheet_name=sheet_name)
+    data.dropna(inplace=True)
+    data.fillna(0, inplace=True)
+    return data
+
+
+# Main page content
+st.markdown("# Main page 🎈")
+st.sidebar.markdown("# Main page 🎈")
+
+
+def main():
+    # st.write("Session State:", st.session_state)
+
+    st.page_link(page="./pages/page_2.py", label="Next Page", icon="❄️", help="Page 2")
+    st.page_link(
+        page="./pages/dummy_page.py", label="Dummy Page", icon="❄️", help="Page Dummy"
+    )
+
+    "Starting a long computation..."
+    # Add a placeholder
+    latest_iteration = st.empty()
+    bar = st.progress(0)
+
+    for i in range(100):
+        # Update the progress bar with each iteration.
+        latest_iteration.text(f"Iteration {i+1}")
+        bar.progress(i + 1)
+        time.sleep(0.01)
+
+    st.write(f'Welcome *{st.session_state.get("name")}*')
+    st.title("Some content")
+
+    map_data = pd.DataFrame(
+        np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4], columns=["lat", "lon"]
+    )
+
+    st.map(map_data)
+
+    x = st.slider("x")  # 👈 this is a widget
+    st.write(x, "squared is", x * x)
+
+    # Add a selectbox to the sidebar:
+    add_selectbox = st.sidebar.selectbox(
+        "How would you like to be contacted?", ("Email", "Home phone", "Mobile phone")
+    )
+
+    # Add a slider to the sidebar:
+    add_slider = st.sidebar.slider("Select a range of values", 0.0, 100.0, (25.0, 75.0))
+
+    chosen = st.sidebar.radio(
+        "Sorting hat",
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"),
+        horizontal=True,
+    )
+    st.sidebar.write(f"You are in {chosen} house!")
+
+    st.text_input("Enter any text", key="input")
+    st.write(st.session_state.get("input"))
+
+    budget = get_data("month")
+    # budget.sort_values("Budget", ascending=False, inplace=True)
+
+    st.markdown("## Monthly Budget")
+    st.write(f"{budget.iloc[3, 0]} = {budget.iloc[3,2]}")
+    st.dataframe(budget)
+
+    # sns plot here for Item agains Budget
+    left, right = st.columns(2)
+
+    fig, ax = plt.subplots()
+    ax = sns.barplot(
+        x="Item",
+        y="Budget",
+        data=budget,
+        palette="pastel",
+    )
+    ax.set(xlabel="Item", ylabel="Budget")
+    plt.xticks(rotation=90)
+    left.pyplot(fig)
+
+    fig, ax = plt.subplots()
+    ax = plt.pie(
+        budget["Budget"],
+        labels=budget["Item"].values,
+        autopct="%1.1f%%",
+        colors=sns.color_palette("pastel"),
+    )
+    right.pyplot(fig)
+
+    flat = get_data("flat")
+
+    st.markdown("## Flat")
+    st.write(f"{flat.iloc[3, 0]} = {flat.iloc[3,2]}")
+    st.dataframe(flat)
+
+    extra = get_data("extra")
+
+    st.markdown("## Extra")
+    st.dataframe(extra)
+
+    explode = [0.1, 0, 0, 0, 0]
+    fig, ax = plt.subplots()
+    ax = plt.pie(
+        extra["Budget"],
+        labels=extra["Item"].values,
+        explode=explode,
+        autopct="%1.1f%%",
+        colors=sns.color_palette("pastel"),
+    )
+    st.pyplot(fig)
+
+
+main()
